94bed2fc6239491a0758967b552db038175b7e19
FCHDev <acetige@gmail.com>
2022-09-13 16:06:57 +0200

    fine tuning / Ajout n° tel au-dessus formulaire

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,10 @@ const Form = () => {
   return (
     <div className="formulaire">
       <h1>Contactez-nous</h1>
+      <span>
+        En remplissant le formulaire ou 📞{" "}
+        <a href="tel:[+33]618377277">06 18 377 277</a>
+      </span>
       <iframe
         src="https://tally.so/embed/mRX4jw?alignCenter=1&hideTitle=1&transparentBackground=1"
         title="formulaire de contact"
